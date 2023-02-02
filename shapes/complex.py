72f08c80b33084ab3f92a76eb54ccdeb6e67e90a
refaeldimri <rafaeldimri@gmail.com>
2023-02-02 19:35:46 +0200

    code is added

--- shapes/complex.py
+++ shapes/complex.py
@@ -2,56 +2,63 @@ from Shape.shapes.detect import *
 from Shape.shapes.calc import *
 
 
-def check_exist_obj_in_list(first_list_of_objects, second_list_of_objects):
+def check_exist_shape_in_list(list_of_shapes, shape_objct):
     """
-    This function get two lists and check if there is same common object(Shape)
+    This function get list and obj and check if this obj exist in the list
     NOTE: These two objects are different, but for us, they are same (because the arrays)
             {"sides": [6, 4, 3], "area": 6, "perimeter": 12}
             {"sides": [6, 3, 4], "area": 6, "perimeter": 12}
-    :param first_list_of_objects:
-    :param second_list_of_objects:
+    :param list_of_shapes:
+    :param shape_objct:
     :return bool:
     """
-    for index, first_item in enumerate(first_list_of_objects):
-        list1_object = set(first_list_of_objects[index].get("sides"))
-        for second_item in second_list_of_objects:
-            list2_object = set(second_item.get("sides"))
-            if list1_object == list2_object:
-                return True
+    for item in list_of_shapes:
+        if set(shape_objct.get("sides")) == set(item.get("sides")):
+            return True
     return False
 
 
-def is_exist(dictionary, list_shapes):
+def is_exist(dictionary, shape_object):
     """
-    This function get dictionary(the values are lists) and list and check
-    if there is same common object in the list and in the dictionary's values
+    This function get dictionary(the values are lists) and object and check
+    if this object exist in the dictionary's list
     :param dictionary:
-    :param list_shapes:
+    :param shape_object:
     :return bool:
     """
-    for list_values in dictionary:
-        new_list_value = dictionary[list_values]
-        flag = check_exist_obj_in_list(new_list_value, list_shapes)
-        if flag:
+    for list_values in dictionary.values():
+        if check_exist_shape_in_list(list_values, shape_object):
             return True
     return False
 
 
 def calculate_shape(list_of_shapes, raise_an_error):
-    dict_shapes = {}
+    dict_shapes = {
+        "rectangles": [],
+        "triangles": [],
+        "squares": []
+    }
     try:
         for shape in list_of_shapes:
-            if raise_an_error:
+            if not raise_an_error:
                 if is_rectangle(shape):
+                    if not is_exist(dict_shapes, shape):
+                        dict_shapes["rectangles"].append(shape)
+                else:
                     raise ValueError("Is not shape")
-
                 if is_triangle(shape):
+                    if not is_exist(dict_shapes, shape):
+                        dict_shapes["triangles"].append(shape)
+                else:
                     raise ValueError("Is not shape")
-
                 if is_square(shape):
+                    if not is_exist(dict_shapes, shape):
+                        dict_shapes["squares"].append(shape)
+                    pass
+                else:
                     raise ValueError("Is not shape")
-              #  else:
-                    # add dict
+            else:
+                return dict_shapes
     except ValueError as e:
         return str(e)
 
--- test/main.py
+++ test/main.py
@@ -15,30 +15,20 @@ if __name__ == '__main__':
             "squares": [{"sides": [2, 2, 1, 1], "area":2, "perimeter":6}, {"sides": [1, 4, 2], "area": 6, "perimeter": 12}],
             "rectangles": [{"sides": [3, 2, 6], "area": 6, "perimeter": 12}, {"sides": [3, 1, 6], "area": 6, "perimeter": 12}]
             }
-    a = [
-        {"sides": [4, 1, 2], "area": 6, "perimeter": 12},
-        {"sides": [6, 2, 1], "area": 6, "perimeter": 12},
-        ]
+    # a = [
+    #     {"sides": [6, 3, 4], "area": 6, "perimeter": 12},
+    #     {"sides": [6, 2, 1], "area": 6, "perimeter": 12},
+    #     ]
 
     # print(dict1.get("triangles"))
-    def check_exist_obj_in_list(first_list_of_objects, second_list_of_objects):
-        for index, first_item in enumerate(first_list_of_objects):
-            list1_object = set(first_list_of_objects[index].get("sides"))
-            for second_item in second_list_of_objects:
-                list2_object = set(second_item.get("sides"))
-                if list1_object == list2_object:
-                    return True
-        return False
+    b = {"sides": [8,9,7], "area": 6, "perimeter": 12}
 
-    def is_exist(dictionary, list_shapes):
-        for list_values in dictionary:
-            new_list_value = dictionary[list_values]
-            flag = check_exist_obj_in_list(new_list_value, list_shapes)
-            if flag:
-                return True
-        return False
+    dict1["triangles"].append({"sides": [8,9,7], "area": 6, "perimeter": 12})
 
-    print(is_exist(dict1, a))
+    a = [[1, 2, 3], [2, 2, 2, 2]]
+    b = [[2, 2, 2, 2], [3, 4, 5], [5, 6, 6], [2, 3, 2, 3], [2, 3, 8], [3, 4, 5]]
+    dict2 = {}
+    print(calculate_shape(b, False))
 
 
 

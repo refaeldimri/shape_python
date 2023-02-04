e96d820255b142a54e951cf46fd4f06aeae7de5f
refaeldimri <rafaeldimri@gmail.com>
2023-02-04 14:13:27 +0200

    final 0402

--- shapes/complex.py
+++ shapes/complex.py
@@ -1,5 +1,6 @@
 import json
 import os
+from collections import Counter
 
 from Shape.shapes.calc import Calc
 import dotenv
@@ -10,11 +11,21 @@ dotenv.load_dotenv()
 
 
 class Complex(object):
+    list_complex_utils = []
 
     def __init__(self):
         self.calc = Calc()
         self.list_complex = json.loads(os.getenv("PARAMETER_LIST_COMPLEX"))
         self.raise_error = int(os.getenv("RAISE_ERROR"))
+
+    @staticmethod
+    def check_exist_shape(item_insert):
+        flag = True
+        for item in Complex.list_complex_utils:
+            if Counter(item_insert) == Counter(item):
+                return True
+        if flag:
+            return False
 
     @staticmethod
     def check_duplicate_shape(list_set, set_shape):
@@ -28,6 +39,12 @@ class Complex(object):
         :return bool:
         """
         return list_set.count(set_shape) == 1
+
+    def check_duplicate_list(self):
+        for item in self.list_complex:
+            if not Complex.check_exist_shape(item):
+                Complex.list_complex_utils.append(item)
+        return Complex.list_complex_utils
 
     def create_object(self, sides):
         """
@@ -53,12 +70,9 @@ class Complex(object):
         """
         This function get list of shape and bool parameter and create
         dictionary of only shape without duplicate
-        :param list_of_shapes:
-        :param raise_an_error:
-        :return:
+        :return dict:
         """
-        s = set()
-        list_of_set = []
+        Complex.list_complex_utils = self.check_duplicate_list()
         dict_shapes = {
             "rectangles": [],
             "triangles": [],
@@ -66,35 +80,20 @@ class Complex(object):
         }
         try:
             if self.raise_error:
-                for shape in self.list_complex:
+                for shape in Complex.list_complex_utils:
                     self.calc.detect.list_of_sides = shape
                     if not self.calc.detect.is_rectangle() and not \
                             self.calc.detect.is_triangle() and not self.calc.detect.is_square():
                         raise ValueError(str(shape) + " Is not shape")
             else:
-                for shape in self.list_complex:
+                for shape in Complex.list_complex_utils:
                     self.calc.detect.list_of_sides = shape
                     if self.calc.detect.is_rectangle():
-                        s = set()
-                        s.add(self.calc.rectangle_area())
-                        s.add(self.calc.rectangle_perimeter())
-                        if not self.check_duplicate_shape(list_of_set, s):
-                            dict_shapes["rectangles"].append(self.create_object(shape))
-                            list_of_set.append(s)
+                        dict_shapes["rectangles"].append(self.create_object(shape))
                     elif self.calc.detect.is_triangle():
-                        s = set()
-                        s.add(self.calc.triangle_area())
-                        s.add(self.calc.triangle_perimeter())
-                        if not self.check_duplicate_shape(list_of_set, s):
-                            dict_shapes["triangles"].append(self.create_object(shape))
-                            list_of_set.append(s)
+                        dict_shapes["triangles"].append(self.create_object(shape))
                     elif self.calc.detect.is_square():
-                        s = set()
-                        s.add(self.calc.square_area())
-                        s.add(self.calc.square_perimeter())
-                        if not self.check_duplicate_shape(list_of_set, s):
-                            dict_shapes["squares"].append(self.create_object(shape))
-                            list_of_set.append(s)
+                        dict_shapes["squares"].append(self.create_object(shape))
                 return dict_shapes
         except ValueError as e:
             return str(e)
--- test/main.py
+++ test/main.py
@@ -1,14 +1,56 @@
 import json
 import os
+from collections import Counter
 
 from Shape.shapes.complex import Complex
+from Shape.shapes.complex import *
 
 
 if __name__ == '__main__':
     cmp = Complex()
     print(type(cmp.calculate_shape()))
     print(cmp.calculate_shape())
-    print(type(json.dumps(os.getenv("RESULT_LIST_COMPLEX"))))
+    # print(type(json.dumps(os.getenv("RESULT_LIST_COMPLEX"))))
+    #
+    # initializing list
+    # test_list = [[2, 2, 2, 2], [3, 4, 5], [5, 6, 6], [2, 3, 2, 3], [2, 3, 8], [3, 4, 5]]
+    # list2 = []
+    # item_insert = [4, 5, 6]
+    # print(cmp.check_duplicate_list())
+   # print(Complex.check_exist_shape([1, 2, 3]))
+
+
+
+    # # printing original list
+    # print("The original list : " + str(test_list))
+    #
+    # # removing duplicate sublist
+    # res1 = []
+    # for i in test_list:
+    #     x = sorted(i)
+    #     res1.append(x)
+    # res = []
+    # for i in res1:
+    #     if tuple(i) not in res:
+    #         res.append(tuple(i))
+    #
+    # # print result
+    # print("The list after duplicate removal : " + str(res))
+    #
+    # for i in range(len(res)):
+    #     res[i] = list(res[i])
+    # print("The list after duplicate removal : " + str(res))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
